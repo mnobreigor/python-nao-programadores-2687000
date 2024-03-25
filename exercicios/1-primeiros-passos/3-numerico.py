85ffabddbb8810402ec6d5f5e1b01f21e7b1b8e9
mnobreigor <164891607+mnobreigor@users.noreply.github.com>
2024-03-25 14:37:34 +0000

    exercício realizado

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,19 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
+nome = "Igor"
+multi = 3
 
+print(nome * multi)
 
 # Realize uma operação entre dados do tipo inteiro e float
+valor = 2
+valor1 = 5
+float1 = 5.5
+
+print((valor * valor1) + float1)
